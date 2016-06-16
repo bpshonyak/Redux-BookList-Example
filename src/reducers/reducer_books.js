c731ab53788fdaf37e4d6a1dd4faa9f996c77ad3
Bogdan Pshonyak <bpshonyak@live.com>
2016-06-16 15:36:41 -0700

    created reducer for books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,18 @@
+// Returns an array of books from the application state.
+
+export defult function() {
+  return [
+    {
+      title: "Superman vs Batman"
+    },
+    {
+      title: "Go man go!"
+    },
+    {
+      title: "Twilight"
+    },
+    {
+      title: "50 Shades of RED"
+    }
+  ]
+}
